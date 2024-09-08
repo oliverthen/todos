92c6f3b030555bf5a3dcfb8c9347f780e0fa0406
Oliver Then <oli171dr@gmail.com>
2024-09-08 12:11:22 -0400

    File cleanup, added main Todos component

---
+++ src/components/Todos.tsx
@@ -0,0 +1,15 @@
+
+
+const Todos: React.FC<{items: string[]}> = (props) =>  {
+	return (
+		<ul>
+			{props.items.map(item => {
+				return (
+					<li key={item}>{item}</li>
+				);
+			})}
+		</ul>
+	);
+}
+
+export default Todos;
